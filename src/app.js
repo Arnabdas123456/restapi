37179511507ca6c764c7cec3a3f9fee06a36056c
Arnab Das <129678209+Arnabdas123456@users.noreply.github.com>
2025-02-27 19:10:36 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("./db/conn");
 const app = express();
 const studentRouter = require("./router/students-route");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || your PORT no;
 
 app.use(express.json());
 app.use(studentRouter);
